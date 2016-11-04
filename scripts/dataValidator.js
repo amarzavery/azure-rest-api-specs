f067e139e87cf3de5894edafec14efb3dbe93b09
Amar Zavery <amzavery@microsoft.com>
2016-11-04 12:18:47 -0700

    added validation for request parameters and responses

---
+++ scripts/dataValidator.js
@@ -0,0 +1,422 @@
+'use strict';
+
+var fs = require('fs'),
+glob = require('glob'),
+path = require('path'),
+util = require('util'),
+async = require('async'),
+moment = require('moment'),
+spec = require('swagger-tools').specs.v2,
+RefParser = require('json-schema-ref-parser');
+
+var cwd = process.cwd();
+var globPath = path.join(__dirname, '../', '/**/swagger/*.json');
+var swaggers = glob.sync(globPath).filter(function(entry) {
+  if (entry.match(/.*arm-storage\/2016-01-01\/swagger.*/ig) !== null) {
+    return entry;
+  }
+});
+
+var specInJson = {}; var refSpecInJson = {};
+
+var constraints = [ 'minLength', 'maxLength', 'minimum', 'maximum', 'enum', 
+  'maxItems', 'minItems', 'uniqueItems', 'multipleOf', 'pattern'];
+
+//Entrypoint of the grunt work.
+swaggers.forEach(function(spec){
+  //parse the spec into a JSON object
+  try {
+    specInJson = parseJSONSync(spec);
+  } catch (err) {
+    throw new Error(util.format('Error occured while parsing the spec "%s" - \n %s.', spec, util.inspect(err, {depth: null})));
+  }
+
+  //unify x-ms-paths into paths
+  if (specInJson['x-ms-paths'] && specInJson['x-ms-paths'] instanceof Object && 
+    Object.keys(specInJson['x-ms-paths']).length > 0) {
+    var paths = specInJson.paths;
+    for (var property in specInJson['x-ms-paths']) {
+      paths[property] = specInJson['x-ms-paths'][property];
+    }
+    specInJson.paths = paths;
+  }
+
+  //Resolve all the file references for scenarios in x-ms-examples. This needs to be done before the spec is provided as an input to 
+  //swagger-tools' validateModel().
+  for (var apiPath in specInJson.paths) {
+    for (var verb in specInJson.paths[apiPath]) {
+      if (specInJson.paths[apiPath][verb]['x-ms-examples']) {
+        //populate all the scenarios for the examples inline.
+        for (var scenarioName in specInJson.paths[apiPath][verb]['x-ms-examples']) {
+          if (!specInJson.paths[apiPath][verb]['x-ms-examples'][scenarioName]["$ref"]) {
+            throw new Error(util.format('$ref not found in %s', specInJson.paths[apiPath][verb]['x-ms-examples'][scenarioName]));
+          }
+          //derefence the scenarioName
+          var scenarioPath = path.resolve(path.dirname(spec), specInJson.paths[apiPath][verb]['x-ms-examples'][scenarioName]["$ref"]);
+          var scenarioData;
+          try {
+            scenarioData = parseJSONSync(scenarioPath);
+          } catch (parsingError) {
+            throw new Error(util.format('Error occured while parsing example data from "%s" - \n%s.', 
+                scenarioPath, util.inspect(parsingError, {depth: null})));
+          }
+          //assign the parsed example data to the scenarioName in the spec, so everything is inline.
+          specInJson.paths[apiPath][verb]['x-ms-examples'][scenarioName] = scenarioData;
+        }
+      } else {
+        console.warn(util.format('Operation - "%s", does not contain the extension: "x-ms-examples".', specInJson.paths[apiPath][verb].operationId));
+      }
+    }
+  }
+
+  async.series([
+    //Get the resolved spec in json (output of the RefParser).
+    function(callback) {
+      process.chdir(path.dirname(spec));
+      RefParser.resolve(specInJson, function(err, result) {
+        process.chdir(cwd);
+        if (err) {
+          return callback(new Error(util.format('Error occurred in resolving the spec "%s". Details of the error are:\n%s.', 
+            spec, util.format(err, {depth: null}))));
+        }
+        refSpecInJson = result;
+        return callback(null, refSpecInJson);
+      });
+    },
+    //Validate every scenario provided in every x-ms-examples extension for every operation.
+    function(callback) {
+      for (var apiPath in specInJson.paths) {
+        for (var verb in specInJson.paths[apiPath]) {
+          if (specInJson.paths[apiPath][verb]['x-ms-examples']) {
+            for (var scenarioName in specInJson.paths[apiPath][verb]['x-ms-examples']) {
+              validateScenario(apiPath, verb, scenarioName, function(err, result) {
+                if (err) {
+                  return callback(new Error(util.format('An error "%s", \n occurred in validating the scenarioName "%s" for operation "%s".',
+                    util.inspect(err, {depth: null}), scenarioName, specInJson.paths[apiPath][verb].operationId)));
+                }
+                if (result) {
+                  console.log(result);
+                }
+              });
+            }
+          }
+        }
+      }
+    }
+  ], function (err, result) {
+    if (err) { console.log(err); return;}
+    console.log(result); return;
+  });
+});
+
+//validates constraints
+function validateConstraints(schema, value, objectName) {
+  constraints.forEach(function (constraintType) {
+    if (schema[constraintType] !== null && schema[constraintType] !== undefined) {
+      if (constraintType.match(/^maximum$/ig) !== null) {
+        if (schema['exclusiveMaximum']) {
+          if (value >= schema[constraintType]) {
+            throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint ' + 
+              '\'exclusiveMaximum\': true and \'maximum\': %s', objectName, value, schema[constraintType]));
+          }
+        } else {
+          if (value > schema[constraintType]) {
+            throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'maximum\': %s', 
+            objectName, value, schema[constraintType]));
+          }
+        }
+      } else if (constraintType.match(/^minimum$/ig) !== null) {
+        if (schema['exclusiveMinimum']) {
+          if (value <= schema[constraintType]) {
+            throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint ' + 
+              '\'exclusiveMinimum\': true and \'minimum\': %s',
+            objectName, value, schema[constraintType]));
+          }
+        } else {
+          if (value < schema[constraintType]) {
+            throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'minimum\': %s', 
+            objectName, value, schema[constraintType]));
+          }
+        } 
+      } else if (constraintType.match(/^maxItems$/ig) !== null) {
+        if (value.length > schema[constraintType]) {
+          throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'maxItems\': %s', 
+          objectName, value, schema[constraintType]));
+        }
+      } else if (constraintType.match(/^maxLength$/ig) !== null) {
+        if (value.length > schema[constraintType]) {
+          throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'maxLength\': %s', 
+          objectName, value, schema[constraintType]));
+        }
+      } else if (constraintType.match(/^minItems$/ig) !== null) {
+        if (value.length < schema[constraintType]) {
+          throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'minItems\': %s', 
+          objectName, value, schema[constraintType]));
+        }
+      } else if (constraintType.match(/^minLength$/ig) !== null) {
+        if (value.length < schema[constraintType]) {
+          throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'minLength\': %s', 
+          objectName, value, schema[constraintType]));
+        }
+      } else if (constraintType.match(/^multipleOf$/ig) !== null) {
+        if (value.length % schema[constraintType] !== 0) {
+          throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'multipleOf\': %s', 
+          objectName, value, schema[constraintType]));
+        }
+      } else if (constraintType.match(/^pattern$/ig) !== null) {
+        if (value.match(schema[constraintType].split('/').join('\/')) === null) {
+          throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'pattern\': %s', 
+          objectName, value, schema[constraintType]));
+        }
+      } else if (constraintType.match(/^uniqueItems/ig) !== null) {
+        if (schema[constraintType]) {
+          if (value.length !== value.filter(function (item, i, ar) { { return ar.indexOf(item) === i; } }).length) {
+            throw new Error(util.format('\'%s\' with value \'%s\' should satify the constraint \'uniqueItems\': %s', 
+          objectName, value, schema[constraintType]));
+          }
+        }
+      } else if (constraintType.match(/^enum/ig) !== null) {
+        var isPresent = schema[constraintType].some(function (item) {
+           return item === value;
+        });
+        if (!isPresent && schema['x-ms-enum'] && !Boolean(schema['x-ms-enum']['modelAsString'])) {
+          throw new Error(util.format('%s is not a valid value for %s. The valid values are: %s', 
+            value, objectName, JSON.stringify(schema[constraintType])));
+        }
+      }
+    }
+  });
+}
+
+//entry point for type validations
+function validateType(schema, data) {
+  if (schema.type.match(/^(number|string|boolean|integer)$/ig) !== null) {
+    if (schema.format) {
+      if (schema.format.match(/^(date|date-time|timespan|duration|date-time-rfc1123|unixtime)$/ig) !== null) {
+        validateDateTypes(schema, data);
+      } else if (schema.format.match(/^byte$/ig) !== null) {
+        validateBufferType(schema, data);
+      } else if (schema.format.match(/^base64url$/ig) !== null) {
+        validateBase64UrlType(schema, data);
+      }
+    } else {
+      validateBasicTypes(schema, data);
+    }
+  } else {
+    throw new Error(util.format('Unknown type: "%s" provided for parameter: "%s"', schema.type, schema.name));
+  }
+};
+
+function validateBasicTypes(schema, value) {
+  if (value !== null && value !== undefined) {
+    if (schema.type.match(/^(number|integer)$/ig) !== null) {
+      if (typeof value !== 'number') {
+        throw new Error(util.format('%s with value %s must be of type number.', schema.name, value));
+      }
+    } else if (schema.type.match(/^string$/ig) !== null) {
+      if (typeof value.valueOf() !== 'string') {
+        throw new Error(util.format('%s with value \'%s\' must be of type string.',  schema.name, value));
+      }
+      if (schema.format && schema.format.match(/^uuid$/ig) !== null) {
+        if (!(typeof value.valueOf() === 'string' && isValidUuid(value))) {
+          throw new Error(util.format('%s with value \'%s\' must be of type string and a valid uuid.',  schema.name, value));
+        }
+      }
+    } else if (schema.type.match(/^boolean$/ig) !== null) {
+      if (typeof value !== 'boolean') {
+        throw new Error(util.format('%s with value %s must be of type boolean.',  schema.name, value));
+      }
+    }
+  }
+}
+
+
+function isValidUuid(uuid) {
+  var validUuidRegex = new RegExp('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$', 'ig');
+  return validUuidRegex.test(uuid);
+};
+
+function validateBufferType(schema, value) {
+  if (value !== null && value !== undefined) {
+    if (!Buffer.isBuffer(value)) {
+      throw new Error(util.format('%s must be of type Buffer.', schema.name));
+    }
+  }
+}
+
+function validateBase64UrlType(schema, value) {
+  if (value !== null && value !== undefined) {
+    if (!Buffer.isBuffer(value)) {
+      throw new Error(util.format('%s must be of type Buffer.', schema.name));
+    }
+  }
+}
+
+function validateDateTypes(schema, value) {
+  if (value !== null && value !== undefined) {
+    if (schema.format.match(/^date$/ig) !== null) {
+      if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+        throw new Error(util.format('%s must be an instanceof Date or a string in ISO8601 format.', schema.name));
+      }
+    } else if (schema.format.match(/^date-time$/ig) !== null) {
+      if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+        throw new Error(util.format('%s must be an instanceof Date or a string in ISO8601 format.', schema.name));
+      }
+    } else if (schema.format.match(/^date-time-rfc-1123$/ig) !== null) {
+      if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+        throw new Error(util.format('%s must be an instanceof Date or a string in RFC-1123 format.', schema.name));
+      }
+    } else if (schema.format.match(/^unixtime$/ig) !== null) {
+      if (!(value instanceof Date || 
+        (typeof value.valueOf() === 'string' && !isNaN(Date.parse(value))))) {
+        throw new Error(util.format('%s must be an instanceof Date or a string in RFC-1123/ISO8601 format ' + 
+          'for it to be serialized in UnixTime/Epoch format.', schema.name));
+      }
+    } else if (schema.format.match(/^(duration|timespan)$/ig) !== null) {
+      if (!moment.isDuration(value)) {
+        throw new Error(util.format('%s must be a TimeSpan/Duration.', schema.name));
+      }
+    }
+  }
+}
+
+// Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
+// because the buffer-to-string conversion in `fs.readFile()`
+// translates it to FEFF, the UTF-16 BOM.
+function stripBOM(content) {
+  if (Buffer.isBuffer(content)) {
+    content = content.toString();
+  }
+  if (content.charCodeAt(0) === 0xFEFF || content.charCodeAt(0) === 0xFFFE) {
+    content = content.slice(1);
+  }
+  return content;
+}
+
+function parseJSONSync(swaggerSpecPath) {
+  return JSON.parse(stripBOM(fs.readFileSync(swaggerSpecPath, 'utf8')));
+}
+
+//wrapper to validateModel of swagger-tools
+function validateModel(modelReference, data, callback) {
+  if (!modelReference) {
+    return callback(new Error('modelReference cannot be null or undefined. It must be of a string. Example: "#/definitions/foo".'))
+  }
+  if (!data) {
+    return callback(new Error('data cannot be null or undefined. It must be of a JSON object or a JSON array.'))
+  }
+  if (!callback) {
+    throw new Error('callback cannot be null or undefined. It must be of a function. Example: function() {}');
+  }
+  spec.validateModel(specInJson, modelReference, data, function (err, result) {
+    if (err) {
+      return callback(err);
+    }
+    if (result) {
+      return callback(null, result);
+    } else {
+      return callback(null, null);
+    }
+  });
+}
+
+function validateNonBodyParameter(schema, data, operationId, scenarioName) {
+  var result = {
+    validationError: null,
+    typeError: null
+  };
+  try {
+    validateConstraints(schema, data, schema.name);
+  } catch (validationError) {
+    result.validationError = validationError.message + ' in operation: \'' + operationId + '\'.';
+  }
+  //type validation
+  try {
+    validateType(schema, data);
+  } catch (typeError) {
+    result.typeError = typeError.message + ' in operation: \'' + operationId + '\'.';
+  }
+  return result; 
+}
+
+//validates different aspects of a given scenario.
+function validateScenario(apiPath, verb, scenarioName, callback) {
+  var operation = specInJson.paths[apiPath][verb];
+  var scenarioData = operation['x-ms-examples'][scenarioName];
+  var validationResult = [];
+  //validate parameters
+  async.forEach(operation.parameters, function (parameter, callback) {
+    var dereferencedParameter = parameter;
+    if (parameter['$ref']) {
+      dereferencedParameter = refSpecInJson.get(parameter['$ref']);
+    }
+    //check for requiredness
+    if (dereferencedParameter.in !== 'body' && dereferencedParameter.required && !scenarioData.parameters[dereferencedParameter.name]) {
+      console.error(util.format('Swagger spec has a parameter named "%s" as required for operation "%s", ' + 
+        'however this parameter is not defined in the example for scenario "%s".', dereferencedParameter.name,
+        operation.operationId, scenarioName));
+    }
+    if (dereferencedParameter.in === 'body') {
+      var modelReference = dereferencedParameter.schema['$ref'];
+      validateModel(modelReference, scenarioData.parameters.body, function(err, result) {
+        if (err) {
+          return callback(err);
+        }
+        if (result) {
+          console.log(util.format('Found errors in validating the body parameter for example "%s" in operation "%s".',
+            scenarioName, operation.operationId));
+          console.log(util.inspect(result, {depth: null}));
+        } else {
+          console.log(util.format('The body parameter for example "%s" in operation "%s" is valid.',
+            scenarioName, operation.operationId));
+        }
+        return callback(result);
+      });
+    } else {
+      var errors = validateNonBodyParameter(dereferencedParameter, scenarioData.parameters[dereferencedParameter.name], 
+        operation.operationId, scenarioName);
+      if (errors.validationError) {
+        console.log(errors.validationError);
+      }
+      if (errors.typeError) {
+        console.log(errors.typeError);
+      }
+      validationResult.push(errors);
+    }
+  }, function(err) {
+    if (err) {
+      return callback(err);
+    }
+  });
+
+  //validate responses
+  async.forEach(Object.keys(operation.responses), function(statusCode, callback) {
+    if (!scenarioData.responses[statusCode]) {
+      return callback(new Error(util.format('Response with statusCode "%s" for operation "%s" is present in the spec, but not in the example "%s".',
+        statusCode, operation.operationId, scenarioName)));
+    }
+    if (operation.responses[statusCode]['schema'] && operation.responses[statusCode]['schema']['$ref']) {
+      var modelReference = operation.responses[statusCode]['schema']['$ref'];
+      validateModel(modelReference, scenarioData.responses[statusCode].body, function(err, result) {
+        if (err) { return callback(err); }
+        if (result) {
+          console.log(util.format('Found errors in validating the response with statusCode "%s" for example "%s" in operation "%s".',
+            statusCode, scenarioName, operation.operationId));
+          console.log(util.inspect(result, {depth: null}));
+        } else {
+          console.log(util.format('Response with statusCode "%s" for example "%s" in operation "%s" is valid.', 
+            statusCode, scenarioName, operation.operationId));
+        }
+      });
+    }
+  }, function(err) {
+    if (err) {
+      return callback(err);
+    }
+  });
+}
+
+
